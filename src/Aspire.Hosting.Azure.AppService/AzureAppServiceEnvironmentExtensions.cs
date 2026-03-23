2590e1495f4f97db4248f9005ad692af0e8285c7
Shilpi Rachna <shilpir@microsoft.com>
2026-03-22 22:32:58 -0700

    Re-enabled autoscale

--- src/Aspire.Hosting.Azure.AppService/AzureAppServiceEnvironmentExtensions.cs
+++ src/Aspire.Hosting.Azure.AppService/AzureAppServiceEnvironmentExtensions.cs
@@ -105,8 +105,11 @@ public static partial class AzureAppServiceEnvironmentExtensions
                 },
                 Kind = "Linux",
                 IsReserved = true,
-                // Enable perSiteScaling so each app service can scale independently
-                IsPerSiteScaling = true
+                // Enable perSiteScaling or automatic scaling so each app service can scale independently
+                IsPerSiteScaling = !resource.EnableAutomaticScaling,
+                IsElasticScaleEnabled = resource.EnableAutomaticScaling,
+                // Capping the automatic scaling limit to 10 as per best practices
+                MaximumElasticWorkerCount = 10
             };
 
             infra.Add(plan);
